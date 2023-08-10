5dc323af567e2581119a89ec3ac633f8d10f8997
aybamz-ois <ahmedsunday14@gmail.com>
2023-08-10 15:03:40 -0700

    t5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this implies main function
+ *
+ * Return: 0 when its right
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
