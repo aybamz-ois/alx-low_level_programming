9ab8a916da3f1e435f543c9cbabcb4411d628510
aybamz-ois <ahmedsunday14@gmail.com>
2023-08-11 16:15:59 -0700

    work 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - its the main function
+ *
+ * Return: everytime 0
+ */
+
+int main(void)
+{
+	char i;
+for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+putchar('\n');
+return (0);
+}
