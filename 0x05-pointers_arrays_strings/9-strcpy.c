06ab6328e59594f6591103b154e23b7fccc8ccd3
aybamz-ois <ahmedsunday14@gmail.com>
2023-08-22 08:31:53 -0700

    r9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @dest: the destination
+ * @src: the source
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0 ; src[a] != '\0' ; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
+}
