d12434747bd675ab0f874509932874f7d8db5881
aybamz-ois <ahmedsunday14@gmail.com>
2023-09-11 14:50:52 -0700

    d0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_STRUCTURE_H
+#define DOG_STRUCTURE_H
+
+/**
+ * struct dog - the structure of the dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
