22ec0f4a4d1462c5408a8854d1a6558d04aec90a
aybamz-ois <ahmedsunday14@gmail.com>
2023-09-26 15:46:39 -0700

    j10q

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * delete_nodeint_at_index - deletes the node at index
  * @head: pointer to the first element in thr list
- * @index: index to to delete note
+ * @index: index of node to be freed
  * Return: 1 or -1
  */
 
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	temp = *head;
 	if (head == NULL || *head == NULL)
 		return (-1);
-	for (a = 0 ; a < index - 1 && temp != 0 ; a++)
+	for (a = 0 ; a < index - 1 && temp != NULL && index != 0 ; a++)
 		temp = temp->next;
 	if (temp == NULL)
 		return (-1);
